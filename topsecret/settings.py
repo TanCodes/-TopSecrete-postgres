09cc1f412f2977a82ea3cf2e9278595f107606e5
TANMAY BARVI <64544882+TanCodes@users.noreply.github.com>
2023-02-22 00:03:21 +0530

    Update settings.py

--- topsecret/settings.py
+++ topsecret/settings.py
@@ -72,18 +72,11 @@ WSGI_APPLICATION = "topsecret.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'top-secrete-DB',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres-tancodes',
+        'NAME': 'DATABASENAME',
+        'USER': 'DATABASEUSERNAME',
+        'PASSWORD': 'DATABASEPASSWORD',
         'HOST': 'localhost',
         'PORT': '5432',
     }
